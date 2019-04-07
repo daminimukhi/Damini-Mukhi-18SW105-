782305175b581ad1a976fc41069bda2337836910
damini mukhi <49147491+daminimukhi@users.noreply.github.com>
2019-04-07 19:28:43 +0500

    Add files via upload

---
+++ Firstproject.java
@@ -0,0 +1,32 @@
+class Firstproject{
+ public static void main(String[] args) {
+ 	String name;
+ 	name="damini mukhi";
+ 	System.out.println(name);
+ 	short totalmarks=600;
+ 	System.out.println("totalmarks = 600");
+ 	long obtainedmarks=550;
+ 	System.out.println("obtainedmarks = 550");
+ 	float percentage =((obtainedmarks*100)/totalmarks);
+ 	System.out.println("percentage =" + percentage);
+ 	char grade='A';
+ 	System.out.println("grade = A");
+    boolean x=false;
+    boolean y=true;
+    if((percentage > 75)==true){
+
+
+    System.out.println("passed");
+   } 
+    else{
+
+    
+    System.out.println("failed");
+}
+
+
+ 	
+
+
+	}
+}
---
+++ task.java
@@ -0,0 +1,11 @@
+class task {
+	public static void main(String[]args){
+	double a=50.25;
+	int b=(int)a;
+	double c=a-b;
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+
+	}
+}
